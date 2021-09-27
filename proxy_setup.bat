63235123c8d912c09a88e54b1b5fe2a8be03f2c8
GiYoung Shin <sudogo00@gmail.com>
2021-09-28 08:35:41 +0900

    Create proxy_setup.bat
    upload for unity setup

---
+++ proxy_setup.bat
@@ -0,0 +1,28 @@
+@echo off
+ :: BatchGotAdmin
+ :-------------------------------------
+ REM  --> Check for permissions
+ >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+ if '%errorlevel%' NEQ '0' (
+     echo Requesting administrative privileges...
+     goto UACPrompt
+ ) else ( goto gotAdmin )
+
+:UACPrompt
+     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+     exit /B
+
+:gotAdmin
+     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+     pushd "%CD%"
+     CD /D "%~dp0"
+ :--------------------------------------  
+
+REM --> set proxy
+setx http_proxy http://10.112.1.184:8080
+setx https_proxy http://10.112.1.184:8080
