c2b053e3e9d72f683462f1f10a41d625de47a250
GiYoung Shin <sudogo00@gmail.com>
2022-02-09 14:25:50 +0900

    Create proxy_setup_tls.bat

---
+++ proxy_setup_tls.bat
@@ -0,0 +1,30 @@
+@echo off
+ :: BatchGotAdmin
+ :-------------------------------------
+ REM  --> Check for permissions
+ >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+ if '%errorlevel%' NEQ '0' (
+     echo Requesting administrative privileges...
+     goto UACPrompt
+ ) else ( goto gotAdmin )
+
+:UACPrompt
+     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+     exit /B
+
+:gotAdmin
+     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+     pushd "%CD%"
+     CD /D "%~dp0"
+ :--------------------------------------  
+
+REM --> set proxy
+setx http_proxy http://10.112.1.184:8080
+setx https_proxy http://10.112.1.184:8080
+setx no_proxy 127.0.0.1,localhost,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
+setx NODE_TLS_REJECT_UNAUTHORIZED 0
